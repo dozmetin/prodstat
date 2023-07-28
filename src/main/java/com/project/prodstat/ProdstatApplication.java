c787594d6e42ba6b2238ac088fdf86043f40f649
Doruk Ozmetin <H.D.Ozmeti@student.tudelft.nl>
2023-07-28 11:56:29 +0300

    Initial commit

---
+++ src/main/java/com/project/prodstat/ProdstatApplication.java
@@ -0,0 +1,13 @@
+package com.project.prodstat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdstatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdstatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/prodstat/ProdstatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.prodstat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdstatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
